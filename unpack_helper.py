60e4cf8c90e7c1838e2cf5e4bc9decd0f18023e8
Andrew Nowak <aacn500@users.noreply.github.com>
2016-08-04 16:16:58 +0100

    Remove unnecessary helper scripts

--- unpack_helper.py
+++
@@ -1,9 +0,0 @@
-def int_to_bytes(n):
-    s = bin(n)[2:]
-    l = [s[:8], s[8:16], s[16:24], s[24:]]
-    return [chr(int(c, 2)) for c in l]
-
-def int_to_bytes(n):
-    s = bin(n)[2:]
-    l = [s[:8], s[8:16], s[16:24], s[24:]]
-    return ''.join([chr(int(c, 2)) for c in l])
